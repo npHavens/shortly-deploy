e198710faee94aefad9809dd897058b1114b1253
Nick Havens <npahavens@gmail.com>
2017-08-21 20:54:45 -0500

    test commit 9

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
         separator: ';',
       },
       dist: {
-        src: ['public/lib/jquery.js', 'public/lib/underscore.js', 'public/lib/handlebars.js', 'public/lib/backbone.js' 'public/client/*.js'],
+        src: ['public/lib/jquery.js', 'public/lib/underscore.js', 'public/lib/handlebars.js', 'public/lib/backbone.js', 'public/client/*.js'],
         dest: 'public/dist/built.js',
       },
     },
@@ -88,9 +88,9 @@ module.exports = function(grunt) {
   });
 
 
-  /////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////
   // Main grunt tasks
-  /////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////
 
 
   grunt.registerTask('test', [
