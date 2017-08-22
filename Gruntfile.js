08ee662a1fe532b6d4abcb54688a6d9c4bca7e85
Nick Havens <npahavens@gmail.com>
2017-08-21 20:57:06 -0500

    test commit 10

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,7 +99,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('build', [
     //runs on server or local-dev
-    'concat', 'cssmin'
+    'concat', 'cssmin', 'uglify'
   ]);
 
   grunt.registerTask('upload', function(n) {
