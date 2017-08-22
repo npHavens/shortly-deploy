2e3ce845f813f4a8e3d8ae886798a49c669920af
Nick Havens <npahavens@gmail.com>
2017-08-21 19:43:58 -0500

    test commit 4

--- Gruntfile.js
+++ Gruntfile.js
@@ -117,10 +117,8 @@ module.exports = function(grunt) {
 
   grunt.registerTask('deploy', [
     // add your deploy tasks here
-    //
     'test', 'build', 'upload:' + grunt.option('prod')
 
   ]);
-
 
 };
